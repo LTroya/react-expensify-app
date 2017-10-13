38f02937d66cc395be6a2d6e2935f7335b641f4d
Luis Troya <troyaluis56@gmail.com>
2017-10-12 21:12:22 -0400

    Remove dummy data from store

--- src/index.js
+++ src/index.js
@@ -13,10 +13,6 @@ import { addExpense } from './actions/expenses';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', note: 'Amount of the invoice', amount: 3000, createdAt: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', note: 'Amount of the invoice', amount: 2500, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', note: 'Amount of the invoice', amount: 109500, createdAt: 0 }));
-
 const jsx = (
     <Provider store={store}>
         <AppRouter/>
