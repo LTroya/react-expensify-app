6f88bb5dad3732063eb262970dde91605f351c6f
Luis Troya <troyaluis56@gmail.com>
2017-10-23 12:33:38 -0400

    Add enviroment variables to use firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 const config = {
-    apiKey: 'AIzaSyAqmou_ud7BIy6lZlaeTKIAVlxZ36KHjsY',
-    authDomain: 'expensify-63e66.firebaseapp.com',
-    databaseURL: 'https://expensify-63e66.firebaseio.com',
-    projectId: 'expensify-63e66',
-    storageBucket: 'expensify-63e66.appspot.com',
-    messagingSenderId: '165764318200',
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 firebase.initializeApp(config);
@@ -15,55 +15,3 @@ firebase.initializeApp(config);
 const database = firebase.database();
 
 export { firebase, database as default };
-
-//database.ref('expenses').on('child_removed', snapshot => {
-//    console.log('snapshot', snapshot.key, snapshot.val());
-//});
-//
-//database.ref('expenses').on('child_changed', snapshot => {
-//    console.log('snapshot', snapshot.key, snapshot.val());
-//});
-//
-//database.ref('expenses').on('child_added', snapshot => {
-//    console.log('snapshot', snapshot.key, snapshot.val());
-//});
-
-//const childSnapshotToObject = (childSnapshot) => {
-//    return { id: childSnapshot.key, ...childSnapshot.val() };
-//};
-
-//database.ref('expenses').on('value', snapshot => {
-//    const expenses = [];
-//
-//    snapshot.forEach(childSnapshot => {
-//        expenses.push(childSnapshotToObject(childSnapshot));
-//    });
-//});
-
-//database.ref('expenses').push({
-//    description: 'Buy a ssd disk',
-//    note: 'This is a ssd disk to improve mac mini performance',
-//    amount: 25300,
-//    createdAt: moment().valueOf()
-//});
-//
-//database.ref('expenses').push({
-//    description: 'Buy a headphones',
-//    note: 'New headphones to improve audio quality for videos',
-//    amount: 5300,
-//    createdAt: moment().valueOf()
-//});
-//
-//database.ref('expenses').push({
-//    description: 'Buy a chair',
-//    note: 'The current chair is not argonomic at all. I need a new one to take care of my back!',
-//    amount: 15300,
-//    createdAt: moment().valueOf()
-//});
-
-//database.ref('notes').push({
-//    title: 'First note',
-//    body: 'This is my note',
-//});
-
-//database.ref('notes').set(notes);
