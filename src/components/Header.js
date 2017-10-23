c871b276f832102e60dd2735fe6d3f275532d076
Luis Troya <troyaluis56@gmail.com>
2017-10-23 12:54:35 -0400

    Remove expense menu option

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
         <h1>Expensify</h1>
         <NavLink activeClassName="is-active" to="/" exact={true}>Dashboard</NavLink>
         <NavLink activeClassName="is-active" to="/create">Create Expense</NavLink>
-        <NavLink activeClassName="is-active" to="/edit">Edit expense</NavLink>
         <NavLink activeClassName="is-active" to="/help">Help</NavLink>
     </header>
 );
