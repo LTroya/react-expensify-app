5fefd8f5b41e62038ebec10859c5e6ef58cba2b8
Luis Troya <troyaluis56@gmail.com>
2017-10-25 23:20:40 -0400

    Style header and summary area

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
 import numeral from 'numeral';
 import getVisiblesExpenses from '../selectors/expenses';
@@ -6,10 +7,16 @@ import getExpensesTotal from '../selectors/expenses-total';
 
 export const ExpensesSummary = ({expensesCount, expensesTotal}) => {
     const expenseWord = expensesCount === 1 ? 'expense' : 'expenses';
+    const formattedExpensesTotal = numeral(expensesTotal).format('$0,0.00');
 
     return (
-        <div>
-            <p>Viewing {expensesCount} {expenseWord} totalling {numeral(expensesTotal).format('$0,0.00')}</p>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{expensesCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span></h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
         </div>
     );
 };
